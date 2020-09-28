c667c08bc0c7775ef17bfbae2e272d6cc4c63e8c
shirjeel-abid <f178032@cfd.nu.edu.pk>
2020-09-28 22:51:46 +0500

    project

---
+++ Assets/HammerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HammerController : MonoBehaviour
+{
+    public Text scoreText;
+    public int score = 0;
+    private MoleSpawner ms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        ms = GetComponent<MoleSpawner>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && ms.gameTime > 0)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos2D = new Vector2(mousePos.x,mousePos.y);
+            RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                Destroy(hit.transform.gameObject);
+                score += 1;
+                scoreText.text = score.ToString();
+                ms.Spawn();
+            }
+        }
+    }
+}
---
+++ Assets/MoleSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoleSpawner : MonoBehaviour
+{
+    public GameObject molePrefab;
+    public Transform[] spawnPoints;
+    public float gameTime;
+    public Text gameText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameTime -= Time.deltaTime;
+
+        if (gameTime < 0)
+        {
+            gameTime = 0;
+        }
+        gameText.text = gameTime.ToString();
+    }
+
+    public void Spawn()
+    {
+        GameObject mole = Instantiate(molePrefab) as GameObject;
+        mole.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+    }
+}
